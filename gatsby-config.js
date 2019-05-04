adf979ebb2b74ed7a453938810481c9ef959cfa5
tmskrtsz <tamas.kertesz@anders.fi>
2019-05-04 12:02:04 +0300

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Starter Mocha',
+    author: 'Tamás Kertész',
+    description: 'Gatsby starter with MDX and styled-components supported out of the box',
+  },
+  plugins: [],
+}

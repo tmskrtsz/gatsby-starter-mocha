eb68758a063bf5c242fb68b896a692f89e03a4fa
tmskrtsz <tamas.kertesz@anders.fi>
2019-05-07 11:04:11 +0300

    Update formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,54 +1,54 @@
 module.exports = {
-  extends: ["standard"],
-  plugins: ["standard", "react"],
+  extends: ['standard'],
+  plugins: ['standard', 'react'],
   rules: {
-    "no-var": "error", // optional, recommended when using es6+
-    "no-unused-vars": 1, // recommended
-    "arrow-spacing": ["error", { before: true, after: true }], // recommended
-    indent: ["error", 2],
-    "no-tabs": 0,
-    "comma-dangle": [
-      "error",
+    'no-var': 'error', // optional, reco1mended when using es6+
+    'no-unused-vars': 1, // recommended
+    'arrow-spacing': ['error', { before: true, after: true }], // recommended
+    indent: ['error', 2],
+    'no-tabs': 0,
+    'comma-dangle': [
+      'error',
       {
-        objects: "always-multiline",
-        arrays: "only-multiline",
-        imports: "never",
-        exports: "never",
-        functions: "never",
+        objects: 'always-multiline',
+        arrays: 'only-multiline',
+        imports: 'never',
+        exports: 'never',
+        functions: 'never',
       },
     ],
 
     // options to emulate prettier setup
-    semi: ["error", "never"],
-    "max-len": ["warn", { code: 120 }],
-    "template-curly-spacing": ["error", "always"],
-    "arrow-parens": ["error", "as-needed"],
+    semi: ['error', 'never'],
+    'max-len': ['warn', { code: 120 }],
+    'template-curly-spacing': ['error', 'always'],
+    'arrow-parens': ['error', 'as-needed'],
 
     // standard.js
-    "space-before-function-paren": [
-      "error",
+    'space-before-function-paren': [
+      'error',
       {
-        named: "always",
-        anonymous: "always",
-        asyncArrow: "always",
+        named: 'always',
+        anonymous: 'always',
+        asyncArrow: 'always',
       },
     ],
 
     // standard plugin - options
-    "standard/object-curly-even-spacing": ["error", "either"],
-    "standard/array-bracket-even-spacing": ["error", "either"],
-    "standard/computed-property-even-spacing": ["error", "even"],
-    "standard/no-callback-literal": ["error", ["cb", "callback"]],
+    'standard/object-curly-even-spacing': ['error', 'either'],
+    'standard/array-bracket-even-spacing': ['error', 'either'],
+    'standard/computed-property-even-spacing': ['error', 'even'],
+    'standard/no-callback-literal': ['error', ['cb', 'callback']],
 
     // react plugin - options
-    "react/jsx-uses-react": "error",
-    "react/jsx-uses-vars": "error",
-    "react/jsx-max-props-per-line": 2,
-    "react/jsx-closing-bracket-location": [1, "line-aligned"],
-    "react/jsx-first-prop-new-line": [1, "multiline-multiprop"],
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+    'react/jsx-max-props-per-line': 2,
+    'react/jsx-closing-bracket-location': [1, 'line-aligned'],
+    'react/jsx-first-prop-new-line': [1, 'multiline-multiprop'],
   },
-  parser: "babel-eslint",
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaVersion: 8, // optional, recommended 6+
   },
-};
+}
